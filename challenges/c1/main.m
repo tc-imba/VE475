3e858ecfa7c3f19411b7783e22c26b993fed7b3c
tc-imba <liuyh615@sjtu.edu.cn>
2017-05-31 17:00:47 +0800

    split the images

--- challenges/c1/main.m
+++ challenges/c1/main.m
@@ -1,1 +1,30 @@
-read('c1.1.images/img26.jpg');
+img_arr=read('c1.1.images/img26.jpg');
+img_arr=[img_arr;read('c1.1.images/img31.jpg')];
+img_arr=[img_arr;read('c1.1.images/img33.jpg')];
+img_arr=[img_arr;read('c1.1.images/img35.jpg')];
+img_arr=[img_arr;read('c1.1.images/img37.jpg')];
+
+black_arr=zeros(1,length(img_arr));
+
+for i=1:length(img_arr)
+    black_arr(i)=sum(sum(img_arr(i,:,:)==0));
+%     break;
+end
+
+img=zeros(640,320);
+num=0;
+
+for i=1:length(img_arr)
+    if black_arr(i)==165&&num<100
+        x=floor(num/10)+1;
+        y=mod(num,10)+1;
+        num=num+1;
+        img((x-1)*64+1:x*64,(y-1)*32+1:y*32)=img_arr(i,:,:);
+    end
+end
+    
+imshow(img);
+
+% small=zeros(0,0);
+% small(:,:)=img_arr(1,:,:);
+% imshow(small);
--- challenges/c1/read.m
+++ challenges/c1/read.m
@@ -1,15 +1,44 @@
-function img=read(path)
+function img_arr=read(path)
     img=imread(path);
     img=rgb2gray(img);
     img=adjust(img,200);
-    
+    %imshow(img);
+    width=32;
+    height=64;
+    block_data=zeros(0,4);
+    num=0;
     rows=read_rows(img);
     for i=1:length(rows)
         cols=read_cols(img(rows(i,1):rows(i,2),:));
         for j=1:length(cols)
-            
+            num=num+1;
+            block_data(num,:)=[rows(i,1),rows(i,2),cols(j,1),cols(j,2)];
         end
     end
+    img_arr=zeros(length(block_data),height,width);
+    for i=1:num
+        img_arr(i,:,:)=crop(img(block_data(i,1):block_data(i,2),block_data(i,3):block_data(i,4)),height,width);
+    end
+end
+
+function img=crop(block,height,width)
+    img=block;
+    s=size(img);
+    if s(1)>height||s(2)>width
+        display('error');
+    end
+    if s(1)<height
+        fill=height-s(1);
+        a=floor(fill/2);b=fill-a;
+        img=[255.*ones(a,s(2)); img; 255.*ones(b,s(2))];
+    end
+    s=size(img);
+    if s(2)<width
+        fill=width-s(2);
+        a=floor(fill/2);b=fill-a;
+        img=[255.*ones(s(1),a), img, 255.*ones(s(1),b)];
+    end
+    %imshow(img);
 end
 
 function rows=read_rows(img)
